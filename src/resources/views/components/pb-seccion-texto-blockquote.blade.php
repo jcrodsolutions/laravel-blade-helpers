a54dcef87767d5ea55aa2e7f282502808792c0e0
Julio C. Rodriguez <jcrodsolutions@gmail.com>
2024-02-16 21:55:55 -0500

    Permite añadir estilos al texto en PbSeccionTextoBlockquote

--- src/resources/views/components/pb-seccion-texto-blockquote.blade.php
+++ src/resources/views/components/pb-seccion-texto-blockquote.blade.php
@@ -2,7 +2,7 @@
 <div class="row pt-3">
     <div class="col-xs-12 col-md-8 offset-md-2">
         <figure class="text-justify d-none d-md-block">
-            <blockquote class="blockquote">
+            <blockquote class="blockquote" style="{{ $estilosDelTexto }}">
                 {!! $texto !!}
             </blockquote>
         </figure>
