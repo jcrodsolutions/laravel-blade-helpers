9cf78b3f861f042e16221f5370ae29c4401d40ab
Julio C. Rodriguez <jcrodsolutions@gmail.com>
2024-03-12 20:50:31 -0500

    Changes in config file to accommodate better usage example using CDN

--- config/blade-helpers.php
+++ config/blade-helpers.php
@@ -13,29 +13,32 @@ return [
     'css' => [
 //        'altcss' => [ 'src' => env('CSS_TEMA') ],
         'bs5' => [
-            'src' => env('CSS_BOOTSTRAP', 'bootstrap.min.css'),
+            'src' => env('BS5_CSS', 'https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css'),
+            'params' => env('BS5_CSS_PARAMS', 'integrity="sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==" crossorigin="anonymous" referrerpolicy="no-referrer"')
         ],
 //        'fontawesome6' => [ 'src' => 'fontawesome6.all.min.css' ],
-        'fontawesome6' => [
-            'src' => "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css",
-            'params' => 'integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer"'
+        'fa6' => [
+            'src' => env('FA6_CSS',"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"),
+            'params' => env('FA6_CSS_PARAMS','integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer"')
         ],
     ],
     'jspath' => 'js/',
     'js' => [
         'bs5' => [
-            'src' => 'bootstrap.bundle.min.js',
+            'src' => env('BS5_JS','https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.bundle.min.js'),
+            'params' => env('BS5_JS_PARAMS ','integrity="sha512-ykZ1QQr0Jy/4ZkvKuqWn4iF3lqPZyij9iRv6sGqLRdTPkY69YX6+7wvVGmsdBbiIfN/8OdsI7HABjvEok6ZopQ==" crossorigin="anonymous" referrerpolicy="no-referrer"')
         ],
 //        'fontawesome6' => 'fontawesome6.all.min.js',
-        'fontawesome6' => [
-            'src' => "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/js/all.min.js",
-            'params' => 'integrity="sha512-rpLlll167T5LJHwp0waJCh3ZRf7pO6IT1+LZOhAyP6phAirwchClbTZV3iqL3BMrVxIYRbzGTpli4rfxsCK6Vw==" crossorigin="anonymous" referrerpolicy="no-referrer"',
+        'fa6' => [
+            'src' => env('FA6_JS',"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/js/all.min.js"),
+            'params' => env('FA6_JS_PARAMS','integrity="sha512-rpLlll167T5LJHwp0waJCh3ZRf7pO6IT1+LZOhAyP6phAirwchClbTZV3iqL3BMrVxIYRbzGTpli4rfxsCK6Vw==" crossorigin="anonymous" referrerpolicy="no-referrer"'),
         ],
         'jquery' => [
-            'src' => "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js",
-            'params' => 'integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"',
+            'src' => env('JQUERY3_JS',"https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"),
+            'params' => env('JQUERY3_JS_PARAMS','integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"'),
         ],
 //        'moment' => [ 'src' => 'moment-2.29.4.min.js' ],
+//         etc...
     ],
     'imgpath' => 'images/',
     'img' => [
